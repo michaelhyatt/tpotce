d23c688a3f2b7aa2dcc45cf7368683bdbe3a63f8
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-09-11 14:54:34 +1000

    pointing at my own repo

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -687,7 +687,7 @@ pip install elasticsearch-curator yq
 
 # Cloning T-Pot from GitHub
 fuBANNER "Cloning T-Pot"
-git clone https://github.com/dtag-dev-sec/tpotce /opt/tpot
+git clone https://github.com/michaelhyatt/tpotce /opt/tpot
 
 # Let's create the T-Pot user
 fuBANNER "Create user"
